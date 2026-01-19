8f5bb008dac495fd37f7ffdea3db26317b1c2839
nikita51bot <89354519+nikita51bot@users.noreply.github.com>
2026-01-19 03:19:09 +0300

    Moved static files to public folder

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to nik51-docs!</h1>
       <p>
-        You can open{' '}
+        It’s pretty empty here for now — I’ll add more content for my projects later. For now, you can just visit{' '}
         <Link href="/docs" className="font-medium underline">
           /docs
-        </Link>{' '}
-        and see the documentation.
+        </Link>
+        .
       </p>
     </div>
   );
